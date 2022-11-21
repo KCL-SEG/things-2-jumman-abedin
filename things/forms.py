e3b3cb7e17af9f858dd84bc6766468a5db183240
Jumman Abedin <k20056501@kcl.ac.uk>
2022-11-21 11:43:49 +0000

    things e1 and e2

--- things/forms.py
+++ things/forms.py
@@ -7,4 +7,4 @@ class ThingForum(forms.ModelForm):
     class Meta:
         model = Thing
         fields = ['name', 'description', 'quantity']
-        widget = {'description': forms.Textarea(), 'quantity': forms.NumberInput()}
+        widgets = {'description': forms.Textarea(), 'quantity': forms.NumberInput()}
