087a23f33563abd3b94fd484c77b0bc2ee6c7886
Jumman Abedin <k20056501@kcl.ac.uk>
2022-11-21 11:05:02 +0000

    things e1 and e2

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,10 @@
 """Forms of the project."""
+from django import forms
+from .models import Thing
 
-# Create your forms here.
+
+class ThingForum(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+        widget = {'description': forms.Textarea(), 'quantity': forms.NumberInput()}
--- things/views.py
+++ things/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .forms import ThingForum
+
 
 def home(request):
+    form = ThingForum
     return render(request, 'home.html', {'form': form})
